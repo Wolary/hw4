3382c0b4586e65b12d2ec2e4a6d8f89436d29b30
Wolary <Ktrnjh19>
2021-03-23 11:11:06 +0300

    Adding new files

--- src/test/java/ObjectTestForm.java
+++ src/test/java/ObjectTestForm.java
@@ -12,6 +12,21 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ObjectTestForm {
+    String  firstName = "Boka",
+            lastName  = "Joka",
+            userEmail = "legenda@mir.com",
+            gender = "Other",
+            userNumber = "8005553535",
+            monthOfBirth = "8",
+            yearOfBirth = "1980",
+            //день хз
+            subject1 = "arts",
+            hobby = "Music",
+            picture = "joka.jpg",
+            currentAddress = "Russia, Krasnodar, Krasnaya 5",
+            state = "ncr",
+            city = "gurgaon";
+
 
     @BeforeAll
     static void setup() {
@@ -24,34 +39,36 @@ public class ObjectTestForm {
         open("https://demoqa.com/automation-practice-form");
 
         //заполняем формы
-        $("#firstName").setValue("Boka");
-        $("#lastName").setValue("Joka");
-        $("#userEmail").setValue("legenda@mir.com");
-        $(byText("Other")).click();
-        $("#userNumber").setValue("8005553535");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(userEmail);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOptionByValue("8");
-        $(".react-datepicker__year-select").selectOptionByValue("1980");
+        $(".react-datepicker__month-select").selectOptionByValue(monthOfBirth);
+        $(".react-datepicker__year-select").selectOptionByValue(yearOfBirth);
         $(".react-datepicker__day.react-datepicker__day--003").click();
-        $("#subjectsInput").setValue("arts").pressEnter();
-        $(byText("Music")).click();
-        $("#uploadPicture").uploadFromClasspath("joka.jpg");
-        $("#currentAddress").setValue("Russia, Krasnodar, Krasnaya 5.");
-        $("#react-select-3-input").setValue("ncr").pressEnter();
-        $("#react-select-4-input").setValue("gurgaon").pressEnter();
+        $("#subjectsInput").setValue(subject1).pressEnter();
+        $(byText(hobby)).click();
+        $("#uploadPicture").uploadFromClasspath(picture);
+        $("#currentAddress").setValue(currentAddress);
+        $("#react-select-3-input").setValue(state).pressEnter();
+        $("#react-select-4-input").setValue(city).pressEnter();
         $("#submit").click();
 
         //проверяем ввод
-        $(".table-responsive").shouldHave(text("Boka Joka"),
-                text("legenda@mir.com"),
-                text("Other"),
-                text("8005553535"),
+        $(".table-responsive").shouldHave(text(firstName + " " + lastName),
+                text(userEmail),
+                text(gender),
+                text(userNumber),
                 text("03 September,1980"),
-                text("Arts"),
-                text("Music"),
-                text("Joka.jpg"),
-                text("Russia, Krasnodar, Krasnaya 5."),
-                text("NCR Gurgaon"));
+                text(subject1),
+                text(hobby),
+                text(picture),
+                text(currentAddress),
+                text(state + " " + city));
+
+        sleep(5000);
     }
 
 }
